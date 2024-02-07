cf7f38454edf059f4e1f41517adcafac1ad30424
Thanh Nguyen <vnscriptkid@gmail.com>
2024-02-08 01:12:02 +0700

    [videoclient] generateUploadUrl

--- video-processing-client/api-service/functions/src/index.ts
+++ video-processing-client/api-service/functions/src/index.ts
@@ -3,6 +3,13 @@ import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
 
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
+
+const storage = new Storage();
+
+const rawVideoBucketName = "video-raw-bucket";
+
 initializeApp();
 
 const firestore = new Firestore();
@@ -17,4 +24,30 @@ export const createUser = functions.auth.user().onCreate((user) => {
   firestore.collection("users").doc(user.uid).set(userInfo);
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
   return;
+});
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+  // Check if the user is authentication
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated."
+    );
+  }
+
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  // Generate a unique filename for upload
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // Get a v4 signed URL for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+
+  return {url, fileName};
 });
--- video-processing-client/api-service/readme.md
+++ video-processing-client/api-service/readme.md
@@ -5,3 +5,12 @@
 - npm install firebase-functions@latest firebase-admin@latest
 - npm run serve
 - firebase deploy --only functions
+
+## permission
+- get svc account of function `generateUploadUrl`
+- grant permission to svc account to upload file to bucket `video-raw-bucket`
+  - go to bucket `video-raw-bucket`
+  - permission > grant access: cloud storage > storage object admin
+- grant permission to svc account create token
+  - go to project > IAM & admin > service account of function `generateUploadUrl` 
+  - add role: service account token creator
