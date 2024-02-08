ab5f4187736be5d49bfab2bd4e6b1698c905910b
Thanh Nguyen <vnscriptkid@gmail.com>
2024-02-08 23:19:35 +0700

    [videoclient] fix build, depl

--- video-processing-client/app/layout.tsx
+++ video-processing-client/app/layout.tsx
@@ -16,11 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-    <body className={inter.className}>
-      <NavBar />
-      {children}
-    </body>
-  </html>
+      <html lang="en">
+        <body className={inter.className}>
+          <NavBar />
+          {children}
+        </body>
+      </html>
   );
 }
--- video-processing-client/app/watch/page.tsx
+++ video-processing-client/app/watch/page.tsx
@@ -1,17 +1,27 @@
 'use client';
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const BUCKET_NAME = 'video-processed-bucket';
 
-export default function Watch() {
+function _Watch() {
   const videoPrefix = `https://storage.googleapis.com/${BUCKET_NAME}/`;
   const videoSrc = useSearchParams().get('v');
 
   return (
     <div>
-      <h1>Watch Page</h1>
-      { <video controls src={videoPrefix + videoSrc}/> }
-    </div>
+        <h1>Watch Page</h1>
+        { <video controls src={videoPrefix + videoSrc}/> }
+      </div>
+  )
+}
+
+export default function Watch() {
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <_Watch />
+    </Suspense>
   );
   }
